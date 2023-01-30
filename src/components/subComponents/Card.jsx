009c599240d9408aa0552a1b9a2afceb534bb1bc
Imtiaz Uddin <iuashrafi@gmail.com>
2023-01-31 03:39:03 +0530

    adding assets,subcomponents

---
+++ src/components/subComponents/Card.jsx
@@ -0,0 +1,33 @@
+const Card = ({ ProjectImg }) => {
+  return (
+    <>
+      <div className="bg-white shadow-lg  rounded-lg ">
+        <img src={ProjectImg} alt="Project Image" className="rounded-lg" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+          <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+            <i class="bi bi-github"></i> Github
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
